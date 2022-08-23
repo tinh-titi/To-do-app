aff42920ea32c3290cbcdf2e195a78a53ab0cef6
Thao Nguyen <nguyenthao.titi@gmail.com>
2022-08-23 23:59:33 +0700

    add textfield

--- src/App.js
+++ src/App.js
@@ -1,12 +1,40 @@
-import Header from './component/Header';
+import { TextField, Typography, styled, Alert } from '@mui/material';
+import React, { useState } from 'react';
+import CustomButton from './component/CustomButton';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <Header></Header>
-    </div>
-  );
-}
+const Wrapper = styled('div')({
+  display: 'flex',
+  justifyContent: 'center'
 
-export default App;
+})
+
+function App(props) {
+  const [input, setInput] = useState()
+  const [data, setData] = useState([])
+
+  function handleOnChange(event) {
+    setInput(event.target.value)
+  }
+
+  function handleOnClick() {
+    setData(data.concat([{ title: input, complete: false}]))
+  }
+
+  function mapData(todo) {
+    return <Alert severity="error">{todo.title}</Alert>
+  }
+  return (
+    <div>
+      <Typography align='center' variant='h3'>TO DO LIST</Typography>
+      <Wrapper>
+        <TextField onChange={handleOnChange} variant='outlined' />
+        <CustomButton onClick={handleOnClick}>Add</CustomButton>
+      </Wrapper>
+      <div>
+        {data.map(mapData)}
+      </div>
+    </div>
+  )
+}
+export default App
---
+++ src/component/CustomButton/index.js
@@ -0,0 +1,7 @@
+import { Button, styled } from "@mui/material";
+
+const CustomButton = styled(Button)({
+    backgroundColor: 'GrayText'
+})
+
+export default CustomButton
