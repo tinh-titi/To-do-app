9723006df303faaac714fe5e1fb08d7f5be7e9d1
Thao Nguyen <nguyenthao.titi@gmail.com>
2022-08-30 23:25:06 +0700

    finished app

--- src/App.js
+++ src/App.js
@@ -1,16 +1,28 @@
-import { TextField, Typography, styled, Alert } from '@mui/material';
+import { TextField, Typography, styled, Alert, } from '@mui/material';
 import React, { useState } from 'react';
 import CustomButton from './component/CustomButton';
 import './App.css';
+import DeleteIcon from './icon/DeleteIcon';
 
 const Wrapper = styled('div')({
   display: 'flex',
   justifyContent: 'center'
+})
+
+const CustomAlert = styled(Alert)({
+  marginBlock: '12px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  "& .MuiAlert-message": {
+    display: 'flex',
+    justifyContent: 'space-between',
+    flexGrow: 1
+  }
 
 })
 
 function App(props) {
-  const [input, setInput] = useState()
+  const [input, setInput] = useState("")
   const [data, setData] = useState([])
 
   function handleOnChange(event) {
@@ -18,17 +30,42 @@ function App(props) {
   }
 
   function handleOnClick() {
-    setData(data.concat([{ title: input, complete: false}]))
+    setData(data.concat([{ title: input, complete: false }]))
+    setInput("")
   }
 
-  function mapData(todo) {
-    return <Alert severity="error">{todo.title}</Alert>
+  // function handleToDoOnClick(index) {
+  //   console.log(index)
+  // }
+
+  const handleToDoOnClick = (index) => {
+    const newData = [...data]
+    newData[index] = { title: newData[index].title, complete: !newData[index].complete }
+    setData(newData)
   }
+
+  const handleDeleteOnClick = (event, index) => {
+    const newData = [...data]
+    newData.splice(index, 1)
+    event.stopPropagation()
+    setData(newData)
+  }
+
+  function mapData(todo, index) {
+    const severity = todo.complete ? "success" : "error"
+    return (
+      <CustomAlert onClick={() => handleToDoOnClick(index)} severity={severity} key={Math.random() * 1000}>
+        {todo.title}
+        <DeleteIcon style={{ width: "20px" }} onClick={(event) => handleDeleteOnClick(event, index)} cursor='pointer'/>
+      </CustomAlert>
+    )
+  }
+
   return (
     <div>
       <Typography align='center' variant='h3'>TO DO LIST</Typography>
       <Wrapper>
-        <TextField onChange={handleOnChange} variant='outlined' />
+        <TextField onChange={handleOnChange} variant='outlined' value={input} />
         <CustomButton onClick={handleOnClick}>Add</CustomButton>
       </Wrapper>
       <div>
---
+++ src/icon/DeleteIcon.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { SvgIcon } from '@mui/material'
+
+export default function DeleteIcon(props) {
+    return (
+        <SvgIcon width="53" height="53" viewBox="0 0 53 53" fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+            <path d="M5 7.5L42 44.5" stroke="black" strokeWidth="3" strokeLinecap="round" />
+            <path d="M2 51.5L51.5 2" stroke="black" strokeWidth="3" strokeLinecap="round" />
+        </SvgIcon>
+    )
+}
